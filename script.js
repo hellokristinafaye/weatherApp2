edf2043f87f5240b511230da1324df458c2b4587
Kristina Faye <hellokristinafaye@gmail.com>
2024-10-31 22:33:47 -0700

    button icon now changes based on which mode we're in.  success

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ var icon = document.getElementById("icon");
 
 icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "icons/sun.png";
+    } else {
+        icon.src = "icons/moon.png";
+        
+    }
 }
 
 // so the return button works inside the input box
